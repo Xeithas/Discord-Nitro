4ab8e3b3f06521e1d8bc71d81abff94284557f98
Vɪɴᴀʏᴀᴋ Pᴀɴᴅᴇʏ <87496159+Harpia-Vieillot@users.noreply.github.com>
2021-08-18 11:40:44 +0530

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,16 @@
+clear
+sec=5
+spinner=(⣻ ⢿ ⡿ ⣟ ⣯ ⣷)
+while [ $sec -gt 0 ]; do
+    echo -ne "\e[33m ${spinner[sec]} Starting dependency installation in $sec seconds...\r"
+    sleep 1
+    sec=$(($sec - 1))
+done
+echo -e "\e[1;32mInstalling Dependencies ---------------------------\e[0m\n" # Don't Remove Dashes / Fix it
+apt-get update
+apt-get upgrade -y
+pkg upgrade -y
+pkg install python wget -y
+wget https://raw.githubusercontent.com/Harpia-Vieillot/Discord-Nitro/Discord.py
+clear
+python3 Discord.py
